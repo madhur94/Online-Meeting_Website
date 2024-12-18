1f29a893f214beeaf14133c0a30d5d40c714b41e
Madhur Patil <madhurpatil5687@gmail.com>
2024-12-18 23:48:59 +0530

    vercel analytics add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react"
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./globals.css";
@@ -41,6 +41,7 @@ export default function RootLayout({
         <body className={`${inter.className} bg-dark-2`}>
           <Toaster />
           {children}
+          <Analytics />
         </body>
       </ClerkProvider>
     </html>
